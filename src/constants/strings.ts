3a6ff65e802f0541ce30b08e47c124629953923f
kr-manish0505 <mkmanish2810@gmail.com>
2025-07-02 23:06:23 +0530

    new  update

---
+++ src/constants/strings.ts
@@ -0,0 +1,30 @@
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/Kr_manisx";
+  static telegramUsername = "Kr_manisx";
+
+  static github = "GitHub";
+  static githubLink = "https://www.github.com/mkv-manish";
+  static githubUsername = "mkv-manish";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/manish-kumar-6959602bb";
+  static linkedInUsername = "manish-kumar-6959602bb";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/kr_manisx";
+  static instagramUsername = "kr_manisx";
+
+  static facbook = "Facbook";
+  static facbookLink = "https://www.facebook.com/profile.php?id=100013929884209&mibextid=ZbWKwL";
+  static facbookUsername = "Manish Paul";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:devmanish2810@gmail.com";
+  static primaryEmail = "devmanish2810@gmail.com";
+
+  static fullName = "Manish Kumar";
+  static shortName = "Kr_manisx";
+}
+
+export default Strings;
